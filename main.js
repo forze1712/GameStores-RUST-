16c3520860b0b546854fa6f97cd1ecf0da24bc34
forze1712 <168270280+forze1712@users.noreply.github.com>
2024-05-09 19:38:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-   // Создаем новый элемент <div>
-    const newBlock = document.createElement('div');
-    // Добавляем содержимое файла в новый блок
-    newBlock.textContent = content;
-    
-    // Получаем ссылку на элемент с id "container"
-    const container = document.getElementById('container');
-    // Вставляем новый блок в конец контейнера
-    container.appendChild(newBlock);
+    console.log("Мой пользовательский метод выполняется!");
