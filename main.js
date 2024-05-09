e5fbfbc87fc020f1cdb80e2bb0c0dfeed8f3880f
forze1712 <168270280+forze1712@users.noreply.github.com>
2024-05-09 20:07:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 var widget = document.createElement('div');
 widget.textContent = 'Это виджет'; // Текст в блоке виджет
 
-// Добавление класса для стилизации извне
-widget.classList.add('widget');
+
 
 // Добавление блока виджет в body документа
 document.body.appendChild(widget);
