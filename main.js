f9627804a35cf3cdae410a76be3b4fdaa6366c95
forze1712 <168270280+forze1712@users.noreply.github.com>
2024-05-09 20:09:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// Создание блока widget
-var widget = document.createElement('div');
-widget.textContent = 'Это виджет'; // Текст в блоке виджет
 
+// Устанавливаем обработчик, который будет вызван после загрузки скрипта
+script.onload = function() {
+    // Создание блока widget
+    var widget = document.createElement('div');
+    widget.textContent = 'Это виджет'; // Текст в блоке виджет
 
+    // Применение класса для стилей
+    widget.classList.add('widget');
 
-// Добавление блока виджет в body документа
-document.body.appendChild(widget);
+    // Добавление блока виджет в body документа
+    document.body.appendChild(widget);
+};
