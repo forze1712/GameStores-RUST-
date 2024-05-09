65918d637aa32b373dde32fe63885b8ed15b3158
forze1712 <168270280+forze1712@users.noreply.github.com>
2024-05-09 16:52:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-12
+   // Создаем новый элемент <div>
+    const newBlock = document.createElement('div');
+    // Добавляем содержимое файла в новый блок
+    newBlock.textContent = content;
+    
+    // Получаем ссылку на элемент с id "container"
+    const container = document.getElementById('container');
+    // Вставляем новый блок в конец контейнера
+    container.appendChild(newBlock);
